a3a8ed4ad3e5e011c664ef675319d907548aab67
ankurlhila <ankur.lhila@gmail.com>
2025-01-02 23:16:41 +0530

    Signup button on navbar updated

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,23 +1,33 @@
-
 import Link from "next/link"
 export default function Navbar() {
   return (
     <div className="flex justify-between h-[10vh] items-center w-[100%]">
-        <div className="flex justify-around">
-            <div className="text-2xl font-polyamine mx-[4vw]">HIVECARE</div>
+      <div className="flex justify-around">
+        <div className="text-2xl font-polyamine mx-[4vw]">HIVECARE</div>
+      </div>
+      <div className="flex justify-around w-[40vw]">
+        <Link href="#" className="font-poppins">
+          About
+        </Link>
+        <Link href="#" className="font-poppins">
+          Features
+        </Link>
+        <Link href="#" className="font-poppins">
+          Testimonials
+        </Link>
+        <Link href="#" className="font-poppins">
+          Security & Privacy
+        </Link>
+      </div>
+      <div className="flex justify-around w-[10vw] font-poppins">
+        <div>
+          <Link href="/signup">Sign Up</Link>
         </div>
-        <div className="flex justify-around w-[40vw]">
-            <Link href="#" className="font-poppins">About</Link>
-            <Link href="#" className="font-poppins">Features</Link>
-            <Link href="#" className="font-poppins">Testimonials</Link>
-            <Link href="#" className="font-poppins">Security & Privacy</Link>
+        <div className="text-white bg-hivegreen rounded-xl px-4 font-poppins">
+          <Link href="/login">Login</Link>
         </div>
-        <div className="flex justify-around w-[10vw] font-poppins">
-            <div >Sign Up</div>
-            <div className="text-white bg-hivegreen rounded-xl px-4 font-poppins"><Link href="/login">Login</Link></div>
-        </div>
-      
+        {/* <div className="text-white bg-hivegreen rounded-xl px-4 font-poppins"><Link href="/login">Login</Link></div> */}
+      </div>
     </div>
   )
 }
-
